9a4d6213170b3596cc21d6f64425635df2b8b374
Eve-Flower <23524499+Eve-Flower@users.noreply.github.com>
2023-09-17 16:09:07 -0400

    count to a number by 2s

---
+++ 2020.py
@@ -0,0 +1,13 @@
+number = 2020
+
+
+def evens(x):
+    counter = 0
+    while counter <= x:
+        if counter <= x:
+            print(counter)
+            counter += 2
+    
+
+evens(number)
+    
